e4b24d83369ec85306b571a138a4aa680ccf1a5a
Unknown <lean1998@hotmail.com>
2017-09-08 11:59:05 -0300

    Auto stash before merge of "master" and "origin/master"

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -8,6 +8,10 @@ class PlayState extends FlxState
 {
 	
 	private var p1:Player;
+	private var enemy1:Enemy;
+	private var enemy2:Enemy;
+	private var enemy3:Enemy;
+	
 	
 	override public function create():Void
 	{
@@ -15,6 +19,12 @@ class PlayState extends FlxState
 		p1 = new Player(19, 14, AssetPaths.Personaje__png);		
 		add(p1);
 		FlxG.camera.bgColor = 0xFF1E00FF;
+		enemy1 = new Enemy(100, 100, AssetPaths.marcianito__png);
+		enemy2 = new Enemy(75, 100, AssetPaths.Marcianito2__png);
+		enemy3 = new Enemy(50, 100, AssetPaths.Marcianito3__png);
+		//add(enemy1);
+		//add(enemy2);
+		//add(enemy3);
 	}
 
 	override public function update(elapsed:Float):Void
