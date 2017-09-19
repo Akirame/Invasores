dea29af5f56d2d4971d765c2a646f5a1d98ea258
Unknown <jquintanamartin@hotmail.com>
2017-09-19 19:13:18 -0300

    Menu (con fanservice para el CID)

---
+++ source/Menu.hx
@@ -0,0 +1,39 @@
+package;
+
+import flixel.FlxG;
+import flixel.FlxState;
+import flixel.text.FlxText;
+
+/**
+ * ...
+ * @author ...
+ */
+class Menu extends FlxState 
+{
+	var texto:FlxText;
+	
+	override public function create():Void
+	{
+		texto = new FlxText (40, 20, 0, "Invaders", 14);
+		add (texto);
+		texto = new FlxText (0, 50, 0, "Hecho por:", 8);
+		add(texto);
+		texto = new FlxText (0, 60, 0, "José Quintana Martín", 8);
+		add(texto);
+		texto = new FlxText (0, 70, 0, "Leandro Rodriguez", 8);
+		add(texto);
+		texto = new FlxText (0, 80, 0, "Gastón Villalba", 8);
+		add(texto);
+		texto = new FlxText (46, 100, 0, "Apretá Enter", 8);
+		add(texto);
+		
+		FlxG.sound.play(AssetPaths.pantalla_de_inicio__wav);
+	}
+	
+	override public function update(elapsed:Float):Void
+	{
+		if (FlxG.keys.justPressed.ENTER)
+			FlxG.switchState(new PlayState());
+	}
+	
+}
