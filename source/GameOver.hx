36ff2320d9c1dc2845bb48a6eafed137994855b6
G <gsvillalba12@hotmail.com>
2017-09-19 20:04:41 -0300

    GameOver y Global

--- source/GameOver.hx
+++ source/GameOver.hx
@@ -6,6 +6,7 @@ import flixel.FlxState;
 import flixel.FlxStrip;
 import flixel.FlxSprite;
 import flixel.text.FlxText;
+import source.Global;
 
 /**
  * ...
@@ -13,13 +14,50 @@ import flixel.text.FlxText;
  */
 class GameOver extends FlxState 
 {
-	var cartelito:FlxText;
+	var cartelito1:FlxText;
+	var cartelito2:FlxText;
+	var cartelito3:FlxText;
+	var conta1:Int;
+	var conta2:Int;
+	var Apagado:Bool;
 
 	override public function create():Void 
 	{
 		super.create();
-		cartelito = new FlxText(44, 68, 0, "perdiste perro", 8);
-		add(cartelito);
+		cartelito1 = new FlxText(5, 30, 0, "Score: " + Global.score, 16);
+		cartelito2 = new FlxText(5, 50, 0, "HighScore: " + (Global.hScore+500) , 16);
+		cartelito3 = new FlxText(22, 90, 0, "GAME OVER", 16);
+		add(cartelito1);
+		add(cartelito2);
+		add(cartelito3);
+		cartelito2.kill();
+		cartelito3.kill();
+		conta1 = 0;
+		conta2 = 0;
+		Apagado = false;
 	}
 	
+	override public function update(elapsed:Float):Void 
+	{
+		super.update(elapsed);		
+ 		if (conta1 == 60 && cartelito2.alive == false)		
+			cartelito2.revive();		 		 	
+		if (conta2 == 40)
+		{
+			if (Apagado == false)
+			{
+				cartelito3.revive();				
+				Apagado = true;
+				conta2 = 0;
+			}
+			else
+			{
+				cartelito3.kill();
+				Apagado = false;
+				conta2 = 0;
+			}
+		}	
+		conta1++;
+		conta2++;
+	}	
 }
---
+++ source/Global.hx
@@ -0,0 +1,18 @@
+package source;
+
+/**
+ * ...
+ * @author G
+ */
+class Global 
+{	
+	static public var lives:Int = 3;
+	static public var score:Int = 0;
+	static public var hScore:Int = 500;
+	
+	public function new() 
+	{
+		
+	}
+	
+}
